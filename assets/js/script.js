a41c9d5af198aaa1264f81eabb248a7d0a127fdb
Akash B <akashb@prdxn.com>
2020-05-22 20:38:36 +0530

    Final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ const user_quotes = document.querySelector(".user-Quotes");
 const Ai_quotes = document.querySelector(".AI-Quotes");
 const user_area = document.querySelector(".user-play-area figure");
 const computer_area = document.querySelector(".computer-play-area figure");
+const user_result = document.querySelector(".user-result");
+const computer_result = document.querySelector(".computer-result");
 const rand_array = ['fa fa-hand-grab-o','fa fa-hand-paper-o','fa fa-hand-scissors-o'];
 let selected_choices = [];
 let counter = 0, user = 0, tie = 0, computer = 0;
@@ -14,6 +16,7 @@ const user_wins = [
 					['fa fa-hand-grab-o', 'fa fa-hand-paper-o'],
 					['fa fa-hand-paper-o', 'fa fa-hand-scissors-o']
 				];
+const rounds = prompt("Enter Number rounds", 0) || 10;
 
 user_choices.forEach( element => {
 	
@@ -28,6 +31,8 @@ user_choices.forEach( element => {
 		winner_choice(selected_choices);
 
 		selected_choices = [];
+
+		(rounds == counter) ? setTimeout(function() {(final_result(), location.reload())}, 1000) : "";
 	})
 });
 
@@ -55,29 +60,6 @@ const recent_result_element = (elemArray, looser) => {
 
 const set_score = (class_name, score) => document.querySelector(class_name).innerHTML = score;
 
-const set_quotes = winner => {
-	switch(winner) {
-   case "computer":
-   		Ai_quotes.innerHTML = random_quote(winner_quotes);
-   		user_quotes.innerHTML = random_quote(looser_quotes);
-   		user_area.classList.add('looser-area');   		
-   		computer_area.classList.remove('looser-area');
-      break;
-   case "user":
-   		Ai_quotes.innerHTML = random_quote(looser_quotes);
-   		user_quotes.innerHTML = random_quote(winner_quotes);
-   		computer_area.classList.add('looser-area');
-   		user_area.classList.remove('looser-area');   		
-      break;
-   default:
-   		Ai_quotes.innerHTML = random_quote(rival_quotes);
-   		user_quotes.innerHTML = random_quote(rival_quotes);
-   		user_area.classList.remove('looser-area');   		
-   		computer_area.classList.remove('looser-area');
-      break;
-	}
-} 
-
 const winner_choice = choiceArray => {
 
 	(choiceArray[0] == choiceArray[1]) ? (tie++, set_score(".tie" ,tie), recent_result_element(choiceArray), set_quotes()) : 
@@ -85,6 +67,37 @@ const winner_choice = choiceArray => {
 		(computer++, set_score(".computer" , computer), recent_result_element(choiceArray, "user"), set_quotes("computer")) :
 		(user++, set_score(".user" , user), recent_result_element(choiceArray, "computer"), set_quotes("user"));
 }
+
+const set_quotes = winner => {
+	switch(winner) {
+   case "computer":
+   		Ai_quotes.innerHTML = random_quote(winner_quotes);
+   		user_quotes.innerHTML = random_quote(looser_quotes);
+   		user_area.classList.add('looser-area');   		
+   		computer_area.classList.remove('looser-area');
+   		user_result.innerHTML = "Looser";
+   		computer_result.innerHTML = "Winner";
+      break;
+   case "user":
+   		Ai_quotes.innerHTML = random_quote(looser_quotes);
+   		user_quotes.innerHTML = random_quote(winner_quotes);
+   		computer_area.classList.add('looser-area');
+   		user_area.classList.remove('looser-area');
+   		user_result.innerHTML = "Winner";
+   		computer_result.innerHTML = "Looser";   		
+      break;
+   default:
+   		Ai_quotes.innerHTML = random_quote(rival_quotes);
+   		user_quotes.innerHTML = random_quote(rival_quotes);
+   		user_area.classList.remove('looser-area');   		
+   		computer_area.classList.remove('looser-area');
+   		user_result.innerHTML = "Tie";
+   		computer_result.innerHTML = "Tie";
+      break;
+	}
+} 
+
+const final_result = () => alert ((computer == user) ? "Match Tie" : (computer > user) ? "computer Wins" : "User Wins");
 
 
 
